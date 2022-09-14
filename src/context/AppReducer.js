2d281966aa52d7e01c09a19be2b6f5fd2287d519
Marko_TON <markotoncanovic@gmail.com>
2022-09-14 16:31:23 +0200

    adding comments

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -14,13 +14,18 @@ export default (state, action) => {
       }
     case 'EDIT_USER':
       const updateUser = action.payload;
+      console.log(updateUser);
 
       const updateUsers = state.users.map(user => {
         if (user.id === updateUser.id) {
+          console.log(updateUser)
+          // Kada naidjes na ovog usera sa ovim ID-em, nemoj njega da vratis pri mapiranju nego vrati updatedUser-a
           return updateUser;
         }
+        console.log(user)
         return user;
       })
+      console.log(updateUsers)
       return {
         ...state,
         users: updateUsers
